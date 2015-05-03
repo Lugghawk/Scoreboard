986eba0e6d4f40efb5410c9c564890e0281e6194
Lugghawk <acidictadpole@gmail.com>
2015-05-03 01:20:27 -0400

    Can now add records via the /score PUT request

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req,res,next){
+  var db = req.db;
+  var collection = db.get('scores');
+  collection.find({}, function(e,docs){
+    res.render('index', {players:docs});
+  });
 });
 
-router.get('/test', function(req,res,next){
+
+router.put('/score', function(req,res,next){
+  var player = req.body;
+  console.log('player: ' +JSON.stringify(player));
   var db = req.db;
-  var collection = db.get('scoreboarditems');
-  collection.find({}, function(e,docs){
-    console.log(JSON.stringify(docs));
-    res.render('index', {title: docs[0].playername});
-  });
+  var dbCollection = db.get('scores');
+   dbCollection.insert(req.body);
+   res.status(200).send();
 });
 
 module.exports = router;
